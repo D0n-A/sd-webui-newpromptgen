b974652f440f1fb3eb7886da66dfd59cfe87af93
Vladimir Mandic <mandic00@live.com>
2023-10-22 14:55:45 -0400

    initial update

--- javascript/promptgen.js
+++ javascript/promptgen.js
@@ -1,22 +1,15 @@
-
-function promptgen_send_to(where, text){
-    textarea = gradioApp().querySelector('#promptgen_selected_text textarea')
+function promptGenSend(text, where){
+    const textarea = gradioApp().querySelector('#promptgen_selected_text textarea')
     textarea.value = text
     updateInput(textarea)
-
     gradioApp().querySelector('#promptgen_send_to_'+where).click()
-
     where == 'txt2img' ? switch_to_txt2img() : switch_to_img2img()
 }
 
-function promptgen_send_to_txt2img(text){ promptgen_send_to('txt2img', text) }
-function promptgen_send_to_img2img(text){ promptgen_send_to('img2img', text) }
-
-function submit_promptgen(){
-    var id = randomId()
+function promptGenSubmit(){
+    const id = randomId()
     requestProgress(id, gradioApp().getElementById('promptgen_results_column'), null, function(){})
-
-    var res = create_submit_args(arguments)
+    const res = create_submit_args(arguments)
     res[0] = id
     return res
 }
